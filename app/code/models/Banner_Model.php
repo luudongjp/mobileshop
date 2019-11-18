5529ef76ecabe9a2fde83f8e03f1a6453a8ab933
namhoang2201 <hoangnamtb95@gmail.com>
2019-11-18 21:33:44 +0700

    update

---
+++ app/code/models/Banner_Model.php
@@ -0,0 +1,6 @@
+<?php
+
+class Banner_Model extends Base_Model
+{
+    protected $table = 'Banner';
+}
--- core/helpers/Url_Helper.php
+++ core/helpers/Url_Helper.php
@@ -1,31 +1,44 @@
-<?php 
-    function baseUrl($uri){
-        $uri = str_replace('?', '&', $uri);
-        $uri_array = explode('/', $uri);
-        $module = $uri_array[0];
-        $action = $uri_array[1];
-        
-        return BASE_URL."?module={$module}&action={$action}";
+<?php
+// function baseUrl($uri){
+//     $uri = str_replace('?', '&', $uri);
+//     $uri_array = explode('/', $uri);
+//     $module = $uri_array[0];
+//     $action = $uri_array[1];
+
+//     return BASE_URL."?module={$module}&action={$action}";
+// }
+function baseUrl($uri)
+{
+
+    return BASE_URL . $uri;
+}
+
+function getParameter($key, $default = null)
+{
+    if (!empty($_GET[$key])) {
+        return $_GET[$key];
     }
 
-    function getParameter($key, $default = null){
-        if(!empty($_GET[$key])){
-            return $_GET[$key];
-        }
+    return $default;
+}
 
-        return $default;
-    }
-    
-    function getPostParameter($key, $default = null){
-        if(!empty($_POST[$key])){
-            return $_POST[$key];
-        }
-
-        return $default;
+function getPostParameter($key, $default = null)
+{
+    if (!empty($_POST[$key])) {
+        return $_POST[$key];
     }
 
-    function redirect($uri){
-        $location = baseUrl($uri);
-        header("location: $location");
-        exit;
-    }
+    return $default;
+}
+
+function redirect($uri)
+{
+    $location = baseUrl($uri);
+    header("location: $location");
+    exit;
+}
+
+function pretty($array_data)
+{
+    print("<pre>" . print_r($array_data, true) . "</pre>");
+}
